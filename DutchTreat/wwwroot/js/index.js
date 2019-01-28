d4e773fd432a94a10630cdc34c70ec51c58749ee
Adam Clarke <adamclarke88@gmail.com>
2019-01-28 16:13:23 +0000

    created project for course

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hellooooo");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying An Item")
+});
+
+
+//var button = document.getElementById("buyButton");
+//button.addEventListener("mouseover", function () {
+//    alert("hovered over button")
+//});
