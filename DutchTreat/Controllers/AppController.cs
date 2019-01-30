e7a0b0b799ddd660bda8953e325248e6ad267d40
Adam Clarke <adamclarke88@gmail.com>
2019-01-30 15:58:48 +0000

    added views/razor pages

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller // AppController derives from Controller
+    { // a controller allows us to map a request that comes in to a specific action, and that action is where specific login will happen
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+
+    }
+}
